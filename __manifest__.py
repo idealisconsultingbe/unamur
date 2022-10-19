5a200be8c1ae51a6b8ab5ec39dd759fd27e191db
raoua <wannass.raoua@gmail.com>
2022-10-19 15:51:28 +0200

    DEM-4 : Expenses

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': "U-namur website",
+    'version': '1.0.0',
+    'summary': """
+        University of Namur Website Module """,
+
+    'description': """
+         This module contains customization on the website module. 
+    """,
+    'author': 'Idealis Consulting',
+    'website': 'http://www.idealisconsulting.com',
+    'category': 'website',
+    'depends': ['account', 'base', 'website', 'hr_expense', 'sale_expense', 'portal', 'sale'],
+
+    'data': [
+        # Data
+        'data/expense_data.xml',
+        # Views
+        'views/expense_templates.xml',
+
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
