a2af8bbf7ed2de825b7746899db54ea8e5eb0e8f
raoua <wannass.raoua@gmail.com>
2022-10-21 13:53:37 +0200

    CPO

--- demo_unamur/models/purchase_order.py
+++ demo_unamur/models/purchase_order.py
@@ -1,4 +1,3 @@
-
 from odoo import fields, models
 
 
@@ -6,3 +5,9 @@ class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
     cpo = fields.Many2one('unamur.cpo', string='CPO')
+
+
+class Users(models.Model):
+    _inherit = "res.users"
+
+    cpo_ids = fields.Many2many('unamur.cpo', string='CPO')
