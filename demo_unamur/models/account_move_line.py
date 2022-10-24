c284da1cd7d422b3f67c893e2d1302dd5306a623
raoua <wannass.raoua@gmail.com>
2022-10-24 09:44:15 +0200

    Expense template

--- demo_unamur/models/account_move_line.py
+++ demo_unamur/models/account_move_line.py
@@ -1,6 +1,12 @@
 from odoo import models, fields, api
 
 
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    journal_code = fields.Char(related="journal_id.code")
+
+
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
