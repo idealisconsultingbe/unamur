1e2abc0dc17c504eb514a76ed01ed0e6c5467104
Idealis Consulting <2088903+idealisconsultingbe@users.noreply.github.com>
2022-10-25 11:48:54 +0200

    [ADD] Smart Analytics

---
+++ dashboard_frame/__manifest__.py
@@ -0,0 +1,42 @@
+{
+    "name": "Smart analytics - Dashboard Frame",
+    'version': '1.0',
+    'category': 'Reporting',
+    "summary": "Dashboards for Smart Analytics",
+    'description': """
+        This module allows the seamless integration of external dashboards in Odoo
+    """,
+    "author": "Idealis Consulting",
+    'website': "https://idealisconsulting.com",
+    "depends": ["base", 'web'],
+    "data": [
+        # Security
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        # Wizards
+        'wizards/create_menu.xml',
+        # Views
+        'views/menuitems.xml',
+        'views/smart_analytics_dashboard.xml',
+        'views/res_config_settings.xml',
+    ],
+    'qweb': [
+        'static/src/xml/smart_analytics.xml',
+    ],
+    "installable": True,
+    'assets': {
+        'web.assets_backend': [
+            'dashboard_frame/static/src/js/smart_analytics_form_renderer.js',
+            'dashboard_frame/static/src/js/smart_analytics_form_view.js',
+            'dashboard_frame/static/src/js/smart_analytics_url2iframe.js',
+        ],
+        'web.assets_frontend': [
+            'dashboard_frame/static/src/js/smart_analytics_form_renderer.js',
+            'dashboard_frame/static/src/js/smart_analytics_form_view.js',
+            'dashboard_frame/static/src/js/smart_analytics_url2iframe.js',
+        ],
+        'web.assets_qweb': [
+            'dashboard_frame/static/src/xml/smart_analytics.xml',
+        ],
+    }
+}
